d02b59a23b5bc17e081538dbcbf5d4a738a8e360
Ricardo Costa <106176120+Ricardo-Cs@users.noreply.github.com>
2025-02-09 12:57:38 -0500

    Update A - Skibidus and Amog'u.cpp

--- codeforces/contests/Round 1003 (Div. 4)/A - Skibidus and Amog'u.cpp
+++ codeforces/contests/Round 1003 (Div. 4)/A - Skibidus and Amog'u.cpp
@@ -1,1 +1,19 @@
+// Link: https://codeforces.com/contest/2065/problem/A
 
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    while(n--) {
+        string text;
+        cin >> text;
+        int size = text.size();
+        
+        text.erase(size - 1);
+        text[size - 2] = 'i'; 
+        cout << text << endl;
+    }
+    return 0;
+}
