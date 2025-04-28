f15cdbc19fa10ff37b61a475d147a229e48fee1d
Ricardo Costa <106176120+Ricardo-Cs@users.noreply.github.com>
2025-04-28 11:44:49 -0500

    Create 2753 - Saida 7.cpp

---
+++ beecrowd/iniciante/2753 - Saida 7.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    for(int i = 97; i <= 122; i++) {
+        // char() - valor alfanumérico do inteiro
+        cout << i << " e " << char(i) << endl;
+    }
+    return 0;
+}
